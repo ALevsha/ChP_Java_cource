3d14124c4fa4b4cda05aaaaccdd8b46d3338f866
ALevsha <levsha26r@gmail.com>
2023-02-07 15:27:49 +0300

    generics 2

--- src/generics/Test1.java
+++ src/generics/Test1.java
@@ -4,10 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Две основные причины создания Generics:
+ *  Type Safe - позволяет создавать списки (коллекции) ОПРЕДЕЛЕННЫХ типов, чтобы в списках не было винигрета;
+ *  Reusable Code - позволяет не создавть похожие классы с определенными одинаковыми методами, они делают класс
+ *  универсальным.
+ */
+
 public class Test1 {
     public static void main(String[] args) {
-        List list = new ArrayList<>(); // нельзя так делать из-за того, что при добавлении объекта другого
-        // класса будет вылетать ошибка при обработке списка (стр. 21)
+        List<String> list = new ArrayList<>(); // нельзя делать список без параметров из-за того,
+        // что при добавлении объекта другого класса будет вылетать ошибка при обработке списка (стр. 14)
 //        list.add("OK");
 //        list.add(1);
 //        list.add(new StringBuilder("asodjawp;d"));
@@ -17,8 +24,8 @@ public class Test1 {
         list.add("Bye");
         list.add("ok");
 
-        for (Object o : list){
-            System.out.println(o + "length: " + ((String)o).length());
+        for (String o : list){
+            System.out.println(o + "length: " + o.length());
         }
     }
 }
